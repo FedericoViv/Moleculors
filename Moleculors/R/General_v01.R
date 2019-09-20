5057dbd9cb8517a3fb46f7cb5b00d88d1a3baaf4
Federico Vivaldi <f.vivaldi1@gmail.com>
2019-09-20 18:39:41 +0200

    Atom counter and graphical matrix begin

--- Moleculors/R/General_v01.R
+++ Moleculors/R/General_v01.R
@@ -44,7 +44,9 @@ Moleculors$molecular_weight = function(){
 
   } else {
 
-    return(message("No Input file detected"))
+    message("No Input file detected")
+
+    return(message("Weight... FAILED"))
   }
 
   weight_vector = vector()
@@ -57,6 +59,61 @@ Moleculors$molecular_weight = function(){
 
   Moleculors$Weight = sum(weight_vector)
 
-  return("Weight .... Ok")
+  return("Weight... Ok")
+
+}
+
+Moleculors$N_atoms = function(){
+
+  if(is.data.frame(Moleculors$Input)){
+
+    Moleculors$Natoms = nrow(Moleculors$Input)
+
+  } else {
+
+    message("No Input file detected")
+
+    return("N° of atoms... FAILED")
+
+  }
+
+  return("N° of atoms... OK")
+
+}
+
+Moleculors$graphical_matrix = function(){
+
+  if(is.data.frame(Moleculors$Input)){
+    hydrogen_vector = c()
+    for (i in 1:nrow(Moleculors$Input)) {
+      if (as.character(Moleculors$Input$Atom[i] == "H")) {
+
+        hydrogen_vector = append(hydrogen_vector, i)
+
+      }
+      if (i == nrow(Moleculors$Input)) {
+
+        Input_H_suppressed = Moleculors$Input[-hydrogen_vector,]
+
+      }
+    }
+
+    for (i in (nrow(Input_H_suppressed):1)) {
+
+      Input_H_suppressed$X[i] = Input_H_suppressed$X[i] - Input_H_suppressed$X[1]
+      Input_H_suppressed$Y[i] = Input_H_suppressed$Y[i] - Input_H_suppressed$Y[1]
+      Input_H_suppressed$Z[i] = Input_H_suppressed$Z[i] - Input_H_suppressed$Z[1]
+      print(Input_H_suppressed)
+    }
+
+    distance_vector = sqrt(apply(apply(Input_H_suppressed[,-1], 1, `^`,2), 2, sum))
+
+  } else {
+
+    message("No Input file detected")
+
+    return("No graphical matrix was computated")
+
+  }
 
 }
