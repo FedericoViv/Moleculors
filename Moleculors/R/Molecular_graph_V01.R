51028564db383f28bfdd78b50b0aa26f9aa8e4be
federico <federico-vivaldi@virgilio.it>
2019-10-03 00:18:46 +0200

    Ematrices done, Vmatrices fixed for lenght 1 bug

--- Moleculors/R/Molecular_graph_V01.R
+++ Moleculors/R/Molecular_graph_V01.R
@@ -30,107 +30,50 @@ Moleculors$graphical_matrix = function(){
       }
     }
 
-    graph_Vdistance_matrix = matrix(nrow = nrow(Input_H_suppressed), ncol = nrow(Input_H_suppressed))
-    graph_Vadj_matrix = matrix(nrow = nrow(Input_H_suppressed), ncol = nrow(Input_H_suppressed))
-    graph_VCdistance_matrix = matrix(nrow = nrow(Input_H_suppressed), ncol = nrow(Input_H_suppressed))
 
+    Moleculors$Vmatrices_copy(Input_H_suppressed)
 
-    for (i in 1:nrow(graph_Vdistance_matrix)) {
-
-      for (j in 1:ncol(graph_Vdistance_matrix)) {
-
-        graph_Vdistance_matrix[i,j] = round(sqrt((Input_H_suppressed$X[j] - Input_H_suppressed$X[i])^2 +
-                                                   (Input_H_suppressed$Y[j] - Input_H_suppressed$Y[i])^2 +
-                                                   (Input_H_suppressed$Z[j] - Input_H_suppressed$Z[i])^2),0)
-
-        graph_VCdistance_matrix[i,j] = nrow(Input_H_suppressed) - round(sqrt((Input_H_suppressed$X[j] - Input_H_suppressed$X[i])^2 +
-                                                                               (Input_H_suppressed$Y[j] - Input_H_suppressed$Y[i])^2 +
-                                                                               (Input_H_suppressed$Z[j] - Input_H_suppressed$Z[i])^2),0)
-
-        if (i == j) {
-          graph_Vadj_matrix[i,j] = 0
-          graph_VCdistance_matrix[i,j] = 0
-        } else if (graph_Vdistance_matrix[i,j] == 1){
-          graph_Vadj_matrix[i,j] = 1
-        } else {
-          graph_Vadj_matrix[i,j] = 0
-        }
-      }
-    }
-
-    edge_matrix = matrix(nrow = nrow(Input_H_suppressed),
-                         ncol = (ncol(Input_H_suppressed)-1))
-    h = 1
-    counter = 1
-
-    for (i in 1:nrow(graph_Vdistance_matrix)) {
-      for (j in h:nrow(graph_Vdistance_matrix)) {
-        if (graph_Vdistance_matrix[i,j] == 1) {
-          edge_matrix[counter, 1] = (Input_H_suppressed$X[i] + Input_H_suppressed$X[j])/2
-          edge_matrix[counter, 2] = (Input_H_suppressed$Y[i] + Input_H_suppressed$Y[j])/2
-          edge_matrix[counter, 3] = (Input_H_suppressed$Z[i] + Input_H_suppressed$Z[j])/2
-          counter = counter + 1
-        }
-      }
-      h = h + 1
-    }
-
-    graph_Edistance_matrix = matrix(nrow = nrow(edge_matrix), ncol = nrow(edge_matrix))
-    graph_Eadj_matrix = matrix(nrow = nrow(edge_matrix), ncol = nrow(edge_matrix))
-
-    for (i in 1:nrow(graph_Edistance_matrix)) {
-      for (j in 1:ncol(graph_Edistance_matrix)) {
-        graph_Edistance_matrix[i,j] = floor(sqrt((edge_matrix[j,1] - edge_matrix[i,1])^2 +
-                                                   (edge_matrix[j,2] - edge_matrix[i,2])^2 +
-                                                   (edge_matrix[j,3] - edge_matrix[i,3])^2))
-
-        if (i == j) {
-          graph_Eadj_matrix[i,j] = 0
-        } else if (graph_Vdistance_matrix[i,j] == 1){
-          graph_Eadj_matrix[i,j] = 1
-        } else {
-          graph_Eadj_matrix[i,j] = 0
-        }
-      }
-    }
-
-    Moleculors$graph_Vdistance_matrix = graph_Vdistance_matrix
-    Moleculors$graph_Vadj_matrix = graph_Vadj_matrix
-    Moleculors$graph_VCdistance_matrix = graph_VCdistance_matrix
-    Moleculors$graph_Edistance_matrix = graph_Edistance_matrix
-    Moleculors$graph_Eadj_matrix = graph_Eadj_matrix
+    Moleculors$Ematrices(Input_H_suppressed, Moleculors$graph_Vadj_matrix)
 
 
   } else {
 
     message("No Input file detected")
 
-    return("No graphical matrix was computed")
+    return("No graphical matrices were computed")
 
   }
 
-  return(message("Computing graphical Matrix... OK"))
+  return(message("Computing graphical Matrices... OK"))
 
 }
 
 
 
 # This function calculate the V matrices using as Input the Hydrogen suppressed
-# cartesian matrix
+# cartesian matrix. In order to compute the distances it starts by calculating the
+# magnitude of the vector from each atom to one another. Then after rounding (too many digits
+# may lead to bad calculation in the following steps) it choose the smallest value (different
+# from 0) has the 1 distance value. A loop is used to assure that every value of lenght 1 has
+# the same value for the normalization step following later.
+# a normalization is the computed and the values are rounded to the upper interger (has the module
+# will be for sure lesser than the real distance). This algoright should be usable for small
+# molecule without many folding. It has been tested for small branched molecule and some cyclic one.
+# What needs to be tested: heteroatoms may change lenght distance, branched molecule with atoms > 20 - 30
 
 Moleculors$Vmatrices = function(Cart_Input_Hsupp){
 
-  graph_Vdistance_matrix = matrix(nrow = nrow(Input_H_suppressed), ncol = nrow(Input_H_suppressed))
-  graph_Vadj_matrix = matrix(nrow = nrow(Input_H_suppressed), ncol = nrow(Input_H_suppressed))
-  graph_VCdistance_matrix = matrix(nrow = nrow(Input_H_suppressed), ncol = nrow(Input_H_suppressed))
+  graph_Vdistance_matrix = matrix(nrow = nrow(Cart_Input_Hsupp), ncol = nrow(Cart_Input_Hsupp))
+  graph_Vadj_matrix = matrix(nrow = nrow(Cart_Input_Hsupp), ncol = nrow(Cart_Input_Hsupp))
+  graph_VCdistance_matrix = matrix(nrow = nrow(Cart_Input_Hsupp), ncol = nrow(Cart_Input_Hsupp))
 
   for (i in 1:nrow(graph_Vdistance_matrix)) {
 
     for (j in 1:ncol(graph_Vdistance_matrix)) {
 
-      graph_Vdistance_matrix[i,j] = sqrt((Input_H_suppressed$X[j] - Input_H_suppressed$X[i])^2 +
-                                                 (Input_H_suppressed$Y[j] - Input_H_suppressed$Y[i])^2 +
-                                                 (Input_H_suppressed$Z[j] - Input_H_suppressed$Z[i])^2)
+      graph_Vdistance_matrix[i,j] = sqrt((Cart_Input_Hsupp$X[j] - Cart_Input_Hsupp$X[i])^2 +
+                                           (Cart_Input_Hsupp$Y[j] - Cart_Input_Hsupp$Y[i])^2 +
+                                           (Cart_Input_Hsupp$Z[j] - Cart_Input_Hsupp$Z[i])^2)
 
 
 
@@ -139,7 +82,17 @@ Moleculors$Vmatrices = function(Cart_Input_Hsupp){
     }
   }
 
-  graph_Vdistance_matrix = ceiling(apply(apply(graph_Vdistance_matrix, 2, round, 2), 2, `/`, min(graph_Vdistance_matrix[1,-1])))
+  graph_Vdistance_matrix = apply(graph_Vdistance_matrix, 2, round, 2)
+
+  for (i in 1:nrow(graph_Vdistance_matrix)) {
+    for (j in 1:nrow(graph_Vdistance_matrix)) {
+      if ((graph_Vdistance_matrix[i,j] - min(graph_Vdistance_matrix[1,-1])) <= 0.05 & i != j) {
+        graph_Vdistance_matrix[i,j] = min(graph_Vdistance_matrix[1,-1])
+      }
+    }
+  }
+
+  graph_Vdistance_matrix = ceiling(apply(graph_Vdistance_matrix, 2, `/`, min(graph_Vdistance_matrix[1,-1])))
   graph_VCdistance_matrix = abs(apply(graph_Vdistance_matrix, 2, `-`, nrow(graph_Vdistance_matrix)))
 
   for (i in 1:nrow(graph_Vdistance_matrix)) {
@@ -160,3 +113,55 @@ Moleculors$Vmatrices = function(Cart_Input_Hsupp){
   Moleculors$graph_VCdistance_matrix = graph_VCdistance_matrix
 
 }
+
+
+
+# This function require the H suppressed cartesian matrix
+# and the adjagency matrix for the vertexes in order to compute
+# the edge cartesian matrix and the relation edges matrices.
+# each matrix will be save in the moleculors scope to be used for
+# future descriptor calculation.
+
+
+Moleculors$Ematrices = function(Cart_Input_Hsupp, Vadjmatrix){
+  edge_matrix = matrix(nrow = nrow(Cart_Input_Hsupp),
+                       ncol = (ncol(Cart_Input_Hsupp)-1))
+  h = 1
+  counter = 1
+
+  for (i in 1:nrow(Vadjmatrix)) {
+    for (j in h:nrow(Vadjmatrix)) {
+      if (Vadjmatrix[i,j] == 1) {
+        edge_matrix[counter, 1] = (Cart_Input_Hsupp$X[i] + Cart_Input_Hsupp$X[j])/2
+        edge_matrix[counter, 2] = (Cart_Input_Hsupp$Y[i] + Cart_Input_Hsupp$Y[j])/2
+        edge_matrix[counter, 3] = (Cart_Input_Hsupp$Z[i] + Cart_Input_Hsupp$Z[j])/2
+        counter = counter + 1
+      }
+    }
+    h = h + 1
+  }
+
+  graph_Edistance_matrix = matrix(nrow = nrow(edge_matrix), ncol = nrow(edge_matrix))
+  graph_Eadj_matrix = matrix(nrow = nrow(edge_matrix), ncol = nrow(edge_matrix))
+
+  for (i in 1:nrow(graph_Edistance_matrix)) {
+    for (j in 1:ncol(graph_Edistance_matrix)) {
+      graph_Edistance_matrix[i,j] = floor(sqrt((edge_matrix[j,1] - edge_matrix[i,1])^2 +
+                                                 (edge_matrix[j,2] - edge_matrix[i,2])^2 +
+                                                 (edge_matrix[j,3] - edge_matrix[i,3])^2))
+
+      if (i == j) {
+        graph_Eadj_matrix[i,j] = 0
+      } else if (Vadjmatrix[i,j] == 1){
+        graph_Eadj_matrix[i,j] = 1
+      } else {
+        graph_Eadj_matrix[i,j] = 0
+      }
+    }
+  }
+
+  Moleculors$graph_Edistance_matrix = graph_Edistance_matrix
+  Moleculors$graph_Eadj_matrix = graph_Eadj_matrix
+}
+
+
